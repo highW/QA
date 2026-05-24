d33e9582228ade7ab7fac6ad47dcb65569ca1a40
highW <valeriygor.01@gmail.com>
2026-05-23 22:42:07 -0700

    block external hosts to fix page load timeout

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,15 @@ module.exports = defineConfig({
     supportFile: 'cypress/support/e2e.js',
     viewportWidth: 1280,
     viewportHeight: 720,
+    pageLoadTimeout: 30000,        // ← don't wait longer than needed
+    blockHosts: [                  // ← block all external CDN requests
+      '*.squarespace.com',
+      '*.sqdcdn.com',
+      '*.googleapis.com',
+      '*.google-analytics.com',
+      '*.googletagmanager.com',
+      '*.facebook.net',
+      '*.opentable.com',
+    ],
   },
 });
