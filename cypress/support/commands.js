a4331133fa4a775aff09733c547ee90ddabd61c3
highW <valeriygor.01@gmail.com>
2026-05-28 23:48:18 -0700

    initial commit

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,3 +6,12 @@ Cypress.Commands.add('fillContactForm', ({ name, email, message }) => {
   cy.get('[data-cy="input-message"]').clear().type(message);
   cy.get('[data-cy="btn-submit"]').click();
 });
+Cypress.Commands.add('shouldHaveValidUrl', { prevSubject: 'element'}, (subject, expectedDomain) =>{
+  cy.wrap(subject)
+  .invoke('attr', 'href')
+  .then((href) => {
+    expect(href, 'href should exist').to.exist;
+    expect(href, 'href should use https').to.match(/^https:\/\//);
+    expect(href, 'href should point to ${expectedDomain}'.to.include(expectedDomain));
+  });
+});
